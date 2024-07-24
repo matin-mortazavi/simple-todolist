89068401cf79a3bb0361ee92d9496a8b56821ff6
Matin Mortazavi <matin.mortazavi.a@gmail.com>
2024-07-24 20:10:44 +0330

    fix (todos-route) : use constant as limit

--- src/routes/_protected/todos/index.tsx
+++ src/routes/_protected/todos/index.tsx
@@ -1,10 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Todos from "@/pages/todos";
 import { todoListOptions } from "@/services/query-options";
+import { todoConstant } from "@/constants";
 
 export const Route = createFileRoute("/_protected/todos/")({
   loader: ({ context: { client } }) => {
-    client.ensureQueryData(todoListOptions(1,6));
+    client.ensureQueryData(todoListOptions(1, todoConstant.INITIAL_LIMIT));
   },
   component: Todos,
 });
